01d87480fc4268391d5346398ea86bf9dcc70405
mawalou14 <awaloum213@gmail.com>
2024-06-16 19:07:02 +0100

    configure text input container

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Colors from './constants/Colors';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.rootContainer}>
+        <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: Colors.white,
-    alignItems: 'center',
-    justifyContent: 'center',
+  rootContainer: {
+    flex: 1
   },
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,9 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import PrimaryButton from "../components/ButtonPrimary";
+import Colors from "../constants/Colors";
 
 function StartGameScreen() {
     return (
-        <View>
-            <Text>Hello</Text>
+        <View style={styles.inputContainer} >
+            <TextInput 
+            style={styles.numberInput}
+            maxLength={2}
+            keyboardType="number-pad"
+            autoCapitalize="none"
+            autoCorrect={false}
+             />
+                    <PrimaryButton>Reset</PrimaryButton>
+                    <PrimaryButton>Confirm</PrimaryButton>
         </View>
     );
 }
@@ -11,5 +21,28 @@ function StartGameScreen() {
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
+    inputContainer: {
+        marginTop: 100,
+        marginHorizontal: 16,
+        padding: 15,
+        backgroundColor: Colors.primary500,
+        borderRadius: 10,
+        elevation: 15, // for android shadow
+        shadowColor: Colors.black,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+    },
 
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: Colors.accent500,
+        borderBottomWidth: 2,
+        color: Colors.accent500,
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
 });
