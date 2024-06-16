432ef88b0fc87bda682ee48c87c4020ad862a134
mawalou14 <awaloum213@gmail.com>
2024-06-16 18:44:52 +0100

    create primary button component

---
+++ components/ButtonPrimary.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+
+
+function PrimaryButton({children}) {
+    return (
+        <View>
+            <Text>{children}</Text>
+        </View>
+    );
+}
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+
+});
