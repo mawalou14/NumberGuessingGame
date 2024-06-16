7743fdd74ef4e06b000d5b6295319158d9c95278
mawalou14 <awaloum213@gmail.com>
2024-06-16 18:36:53 +0100

    add basic colors

---
+++ constants/Colors.js
@@ -0,0 +1,12 @@
+
+const Colors = {
+    primary500: '#72063c',
+    primary600: '#640233',
+    primary800: '#4e0329',
+    primary800: '#3b021f',
+    accent500: '#ddb52f',
+    white: '#ffffff',
+    black: '#000000',
+}
+
+export default Colors;
