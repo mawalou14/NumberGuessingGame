03a89ba6facb97e8b2380c5f33a7c711b000d403
mawalou14 <awaloum213@gmail.com>
2024-06-17 15:09:04 +0100

    created the number container component

---
+++ components/game/NumberContainer.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+import Colors from "../../constants/Colors";
+
+
+function NumberContainer({ children }) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.numberText}>{children}</Text>
+        </View>
+    );
+}
+
+export default NumberContainer;
+
+const styles = StyleSheet.create({
+    container: {
+        borderWidth: 4,
+        borderColor: Colors.accent500,
+        padding: 24,
+        margin: 25,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    numberText: {
+        color: Colors.accent500,
+        fontSize: 36,
+        fontWeight: 'bold',
+    }
+})
